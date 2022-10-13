5e28f81a46ccc913f00969b4389e69ddaf7eaabf
2006uri <115743876+2006uri@users.noreply.github.com>
2022-10-13 16:36:42 -0500

    Create Actividad 1.py

---
+++ Actividad 1.py
@@ -0,0 +1,15 @@
+from random import random randit
+dim = 5
+Largo = 10
+pos = 0 dim
+
+
+def paso (pas, dim)
+    d = randit (0, dim-1)
+    pos d += -1  if  random () < 0.5 else 1
+    return pos
+    
+    
+    for t in  range(largo):
+        pos  = paso (pas, dim)
+        print(pas)
